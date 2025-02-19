94c354fd78752bf281b0256a8b6eca9b9d8c1d34
Sarvesh Vadagave <149609776+sarveshv25@users.noreply.github.com>
2025-02-19 22:03:10 +0530

    Add files via upload

---
+++ Linked list.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = newNode;
+    }
+
+    void deleteFromBeginning() {
+        if (!head) return;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void deleteFromEnd() {
+        if (!head) return;
+        if (!head->next) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next->next) temp = temp->next;
+        delete temp->next;
+        temp->next = nullptr;
+    }
+
+    void display(const string& msg) {
+        cout << msg;
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    list.insert(5);
+    list.insert(6);
+    
+    list.display("Original list: ");
+    
+    list.deleteFromBeginning();
+    list.display("List after deleting a node at the beginning: ");
+    
+    list.deleteFromEnd();
+    list.display("List after deleting a node at the end: ");
+    
+    return 0;
+}
+
